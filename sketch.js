707e3368e2fbd08d7c739019d6c928be6c681807
kristian003 <kristian003@gmail.com>
2016-09-08 08:29:17 -0400

    BlackBacground

--- sketch.js
+++ sketch.js
@@ -2,13 +2,13 @@ var circleX = 0;
 
 function setup() {
   createCanvas (900,900);
-  background (200);
+  background (0);
 }
 
 function draw() {
   //background (200);
   noStroke();
-  fill(250,200,200,25);
+  fill(250,200,255,100);
   ellipse (mouseX,mouseY,25,25);
 
 }
