2621e9ca2b38a91f17db4b5d93d962295ed5efd6
kristian003 <kristian003@gmail.com>
2016-09-08 08:20:20 -0400

    Func_mousePressed_pinkellipse

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var circleX = 0;
+
+function setup() {
+  createCanvas (900,900);
+  background (200);
+}
+
+function draw() {
+  //background (200);
+  noStroke();
+  fill(250,200,200,25);
+  ellipse (mouseX,mouseY,25,25);
+
+}
+
+function mousePressed () {
+background (200);
+}
